838e2b7c804d1b40fdc822c8b6615b1b0269a7ce
Lawrenci01 <rlnodado@gbox.ncf.edu.ph>
2024-12-12 15:34:46 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,16 +3,18 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-		// Get rid of the CORS error
-		proxy: {
-			"/api": {
-				target: "http://localhost:4000",
-				changeOrigin: true,
-				secure: false,
-			},
-		},
-	},
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:4000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  build: {
+    outDir: 'public',  // Change the output directory to 'public'
+  },
 });
