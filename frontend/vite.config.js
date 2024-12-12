d310161ea71dfccf560ffc9ddb76f019e8a1b4ec
lawrence nodado <rlnodado@gbox.ncf.edu.ph>
2024-12-12 20:55:18 +0800

    Update: description of changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,42 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-		// Get rid of the CORS error
-		proxy: {
-			"/api": {
-				target: "http://localhost:4000",
-				changeOrigin: true,
-				secure: false,
-			},
-		},
-	},
+  plugins: [
+    react(),
+    ...(process.env.NODE_ENV === 'production'
+      ? [visualizer({ filename: './stats.html', open: true })]
+      : []),
+  ],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_URL || 'http://localhost:4000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    emptyOutDir: true,
+    chunkSizeWarningLimit: 500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
+  publicDir: 'public',
 });
